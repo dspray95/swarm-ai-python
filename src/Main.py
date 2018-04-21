156b072689badb2542b4cc8032b3733a7d33bc53
dspray95 <dspray95@gmail.com>
2018-04-21 15:27:54 +0100

    added running simulation functionality

--- src/Main.py
+++ src/Main.py
@@ -5,9 +5,10 @@ from src.Runner import Runner
 
 path = "C:\\Users\\User\\Documents\\" + datetime.datetime.now().strftime("%Y-%M-%d-%H-%M-%S")
 
-runner = Runner(path)
+runner = Runner(path, "C:\\Users\\User\\IdeaProjects\\swarm-ai-bee-behaviour\\out\\artifacts\\swarm_ai_bee_behaviour_jar\\swarm-ai-bee-behaviour.jar")
 reader = Reader(path)
 writer = Writer(path)
 
-reader.read_files()
-writer.write_file(reader.aggression, reader.distribution, reader.dead, reader.productivity, reader.time)
+runner.run_for_count(2)
+# reader.read_files()
+# writer.write_file(reader.aggression, reader.distribution, reader.dead, reader.productivity, reader.time)
--- src/Runner.py
+++ src/Runner.py
@@ -1,7 +1,20 @@
+import subprocess
+
+
 class Runner:
 
-    def __init__(self, path):
+    def __init__(self, path, jar_path):
         self.path = path + "\\output"
+        self.jar_path = jar_path
 
-    def run(self):
-        return self
+    def run_for_count(self, count):
+        args = "java -jar " + self.jar_path + " write_log_file"
+        for i in range(0, count):
+            print("sim " + str(i))
+            try:
+                subprocess.Popen(subprocess.call(args, shell=True))
+            except TypeError:
+                print("finished sim " + str(i))
+
+
+
